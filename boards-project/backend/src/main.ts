94ccd7881fcb2a943428ab2e7cf0042997167998
alikhanabu <alikhanabu1704@gmail.com>
2026-05-03 02:00:05 +0500

    fix require esm main

--- boards-project/backend/src/main.ts
+++ boards-project/backend/src/main.ts
@@ -1,21 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import * as cookieParser from 'cookie-parser';
-
-const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.use(cookieParser());
+  const app = await NestFactory.create(AppModule);
+
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  app.use(require('cookie-parser')());
+
   app.enableCors({
     origin: ['http://localhost:8000', 'https://tasknest-56zy.vercel.app'],
     credentials: true,
   });
+
   await app.init();
+  return app;
+}
+
+let cachedApp: any;
+
+export default async function handler(req: any, res: any) {
+  if (!cachedApp) {
+    cachedApp = await bootstrap();
+  }
+  const httpAdapter = cachedApp.getHttpAdapter();
+  httpAdapter.getInstance()(req, res);
 }
 
 bootstrap();
-
-export default server;
