f6d9b5730750cb8a3ddbd81cd55faf7faf3366c0
alikhanabu <alikhanabu1704@gmail.com>
2026-05-03 01:44:37 +0500

    fix nestjs serverless

--- boards-project/backend/src/main.ts
+++ boards-project/backend/src/main.ts
@@ -2,16 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
+let app: any;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
-  app.enableCors({
-    origin: [
-      'http://localhost:8000',
-      'https://https://tasknest-56zy.vercel.app/login.vercel.app',
-    ],
-    credentials: true,
-  });
-  await app.listen(process.env.PORT || 7000);
+  if (!app) {
+    app = await NestFactory.create(AppModule);
+    app.use(cookieParser());
+    app.enableCors({
+      origin: ['http://localhost:8000', 'https://tasknest-56zy.vercel.app'],
+      credentials: true,
+    });
+    await app.init();
+  }
+  return app;
 }
+
 bootstrap();
+
+export default async function handler(req: any, res: any) {
+  const app = await bootstrap();
+  const httpAdapter = app.getHttpAdapter();
+  httpAdapter.getInstance()(req, res);
+}
